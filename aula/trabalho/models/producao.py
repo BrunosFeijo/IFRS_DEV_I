e12b3aefb6f3919f510dacce7282c694e0d3c350
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-04-08 19:40:06 -0300

    add-genero_in_producao

--- aula/trabalho/models/producao.py
+++ aula/trabalho/models/producao.py
@@ -1,10 +1,12 @@
 from trabalho.models.base import *
 
+from aula.trabalho.models.genero import Genero
+
 
 class Producao(BaseModel):
     titulo = models.CharField(min_length=1, max_length=200)
     sinopse = models.CharField(min_length=10, max_length=500)
-    genero = ...
+    genero = models.CharField(max_length=50, choices=[(tag.name, tag.value) for tag in Genero])
     classificacao = models.CharField(min_length=1, max_length=10)
     nota = models.FloatField(default=0)
     duracao = ...
