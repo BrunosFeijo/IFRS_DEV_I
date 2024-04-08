f3524dd34f2f8e6099ee5b34ec957f797ce229a7
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-04-08 14:22:02 -0300

    add-fields-producao

--- aula/trabalho/models/producao.py
+++ aula/trabalho/models/producao.py
@@ -2,10 +2,18 @@ from trabalho.models.base import *
 
 
 class Producao(BaseModel):
-    nome = models.CharField(max_length=200)
+    titulo = models.CharField(min_length=1, max_length=200)
+    sinopse = models.CharField(min_length=10, max_length=500)
+    genero = ...
+    classificacao = models.CharField(min_length=1, max_length=10)
+    nota = models.FloatField(default=0)
+    duracao = ...
+    popularidade = models.IntegerField(default=0)
+    especificacoes = models.CharField(min_length=5, max_length=50)
+    data_lancamento = ...
 
     class Meta:
         abstract = False
 
     def __str__(self):
-        return self.nome
+        return self.titulo
