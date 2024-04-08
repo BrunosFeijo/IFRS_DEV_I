b6c5817a3afae5a497ff255b9b04c9febe8227f6
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-04-08 20:05:41 -0300

    corrections

--- aula/exemplo/models/example.py
+++ aula/exemplo/models/example.py
@@ -1,11 +1,16 @@
 from exemplo.models.base import *
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Example(BaseModel):
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200,
+                            help_text="Nome do clube")
+    torcedores = models.IntegerField(default=0,
+                                     help_text="Quantidade de Torcedores",
+                                     validators=[MinValueValidator(0)])
 
     class Meta:
         abstract = False
 
     def __str__(self):
-        return self.nome
+        return f"{self.nome}: {self.torcedores}"
--- aula/trabalho/models/producao.py
+++ aula/trabalho/models/producao.py
@@ -1,17 +1,16 @@
 from trabalho.models.base import *
-
-from aula.trabalho.models.genero import Genero
+# from .genero import Genero
 
 
 class Producao(BaseModel):
-    titulo = models.CharField(min_length=1, max_length=200)
-    sinopse = models.CharField(min_length=10, max_length=500)
-    genero = models.CharField(max_length=50, choices=[(tag.name, tag.value) for tag in Genero])
-    classificacao = models.CharField(min_length=1, max_length=10)
+    titulo = ... # models.CharField(min_length=1, max_length=200)
+    sinopse = ... # models.CharField(min_length=10, max_length=500)
+    genero = ... # models.CharField(max_length=50, choices=[(tag.name, tag.value) for tag in Genero])
+    classificacao = ... # models.CharField(min_length=1, max_length=10)
     nota = models.FloatField(default=0)
     duracao = ...
     popularidade = models.IntegerField(default=0)
-    especificacoes = models.CharField(min_length=5, max_length=50)
+    especificacoes = ... # models.CharField(min_length=5, max_length=50)
     data_lancamento = ...
 
     class Meta:
