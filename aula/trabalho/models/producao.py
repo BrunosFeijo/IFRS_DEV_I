2d0eee89bff2ab22f21f95f2a6cc0d10d141a8e6
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-04-14 23:28:01 -0300

    create-script-example

--- aula/trabalho/models/producao.py
+++ aula/trabalho/models/producao.py
@@ -1,21 +1,26 @@
-from django.core.validators import MinLengthValidator
+import time
+from datetime import timezone
+
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from trabalho.models.base import *
 # from .genero import Genero
 
 
 class Producao(BaseModel):
-    titulo = models.CharField(max_length=200, validators=[MinLengthValidator(1)])
+    titulo = models.CharField(max_length=100, validators=[MinLengthValidator(1)])
     sinopse = models.CharField(max_length=500, validators=[MinLengthValidator(10)])
     genero = ... # models.CharField(max_length=50, choices=[(tag.name, tag.value) for tag in Genero])
     classificacao = models.CharField(max_length=10, validators=[MinLengthValidator(1)])
-    nota = models.FloatField(default=0)
-    duracao = ...
-    popularidade = models.IntegerField(default=0)
+    quantidade_avaliacoes: models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    nota = models.FloatField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    duracao = models.TimeField()
+    popularidade = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
     especificacoes = models.CharField(max_length=50, validators=[MinLengthValidator(5)])
-    data_lancamento = ...
+    data_lancamento = models.DateField()
 
     class Meta:
         abstract = False
 
     def __str__(self):
         return self.titulo
+
